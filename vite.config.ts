4d521f29df67b7b9f2a9da7df59ccca471745a0e
Ameerul Mujahidin <ameerulmujahidin96@gmail.com>
2026-04-21 10:41:50 +0400

    Set base path to '/' for all modes

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/mujahidin-nexus/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
