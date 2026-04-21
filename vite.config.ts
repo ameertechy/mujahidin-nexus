6345e873d7d6d55ac861af2c3a8577768f242972
Ameerul Mujahidin <ameerulmujahidin96@gmail.com>
2026-04-21 11:07:34 +0400

    Update base path for development and production modes

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: mode === "development" ? "/" : "/mujahidin-nexus/",
   server: {
     host: "::",
     port: 8080,
